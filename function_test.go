9e379f5e3bf1c62418f225ec3b48f0d7d1a614fe
Yu YAGIHASHI <yagihash@ssrf.dev>
2023-05-09 12:08:16 +0900

    add function_test

---
+++ function_test.go
@@ -0,0 +1,31 @@
+package function
+
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestNextMonth(t *testing.T) {
+	cases := []struct {
+		y  int
+		m  int
+		ny int
+		nm int
+	}{
+		{2000, 1, 2000, 2},
+		{2000, 12, 2001, 1},
+	}
+
+	for _, c := range cases {
+		ny, nm := NextMonth(c.y, c.m)
+
+		if diff := cmp.Diff(ny, c.ny); diff != "" {
+			t.Errorf("unexpected next year\n%s", diff)
+		}
+
+		if diff := cmp.Diff(nm, c.nm); diff != "" {
+			t.Errorf("unexpected next month\n%s", diff)
+		}
+	}
+}
